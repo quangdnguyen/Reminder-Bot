2f001b0cab57dcc863c2a00aab52975df4dc6e21
Quang Nguyen <qdn8mj@virginia.edu>
2019-07-24 15:32:10 -0400

    typo fix

--- bot.js
+++ bot.js
@@ -7,7 +7,7 @@ var OWEN_ID = 39868511;
 
 function respond() {
   var request = JSON.parse(this.req.chunks[0]),
-    botRegex = /^\/Parents Formal$/,
+    botRegex = /^\/Parents Formal$/;
 
   if (request.text && botRegex.test(request.text)) {
     this.res.writeHead(200);
