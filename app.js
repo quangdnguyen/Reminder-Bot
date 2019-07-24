0f16cbc179c2210a818896ef4d4a8dc514feef09
Quang Nguyen <qdn8mj@virginia.edu>
2019-07-24 16:36:23 -0400

    code clean-up

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ var http, director, bot, router, server, port;
 
 http        = require('http');
 director    = require('director');
-//cool        = require('cool-ascii-faces');
 bot         = require('./bot.js');
 
 router = new director.http.Router({
